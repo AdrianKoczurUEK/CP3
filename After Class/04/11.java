a285e9430f58d07a6b34cf47d20b057397a1c2f4
AdrianKoczurUEK <s219487@student.uek.krakow.pl>
2021-10-28 21:12:19 +0200

    04/AC/11

---
+++ After Class/04/11.java
@@ -0,0 +1,32 @@
+class Product {
+
+    String name;
+    boolean vegetarian;
+    
+    Product(String name, boolean vegetarian){
+        setName(name);
+        setVegetarian(vegetarian);
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    void setVegetarian(boolean vegetarian){
+        this.vegetarian = vegetarian;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    boolean getVegetarian(){
+        return vegetarian;
+    }
+
+    public static void main(String[] args) {
+        Product p1 = new Product("Kebab",false);
+        System.out.println("Product name: "+p1.getName());
+        System.out.println("Vegetarian? : "+p1.getVegetarian());
+    }
+}
